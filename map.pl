21eed6c478e8390c72dffa590550c549e75c43e6
Jesse Morgan <morgajel@gmail.com>
2012-09-29 21:13:49 +0000

    HUUUGE change to move most of the city gen code to a perl module so I can share the coade with the map

--- map.pl
+++ map.pl
@@ -6,14 +6,44 @@ use GD;
 use List::Util 'shuffle', 'min', 'max' ;
 use POSIX;
 use XML::Simple;
+###################################################
+# Using findbin to locate our new City.pm module
+use FindBin qw/$Bin/;
+BEGIN {
+    # A rather useless regex, but whatever. 
+    # Who woulda throught checking for taint stinks? :D
+    if ($Bin =~ m!([\w\.\-/]+)!) {
+        $Bin = $1;
+    } else {
+        print STDOUT "Bad directory $Bin\n";
+    }
+}
+use lib "$Bin/";
+###################################################
+use City 'build_city', 'd','rand_from_array', 'rand_from_array' ;
 
+my $xml = new XML::Simple;
+
+###########################################################
+# Yes, this is sloppy. I am aware, but it's also unique.
+# Unique, Ubiquitous Singletons.
 our $q = CGI->new;
-our $seed=set_seed();
-srand $seed;
+our $xml_data = $xml->XMLin(   "../data.xml", ForceContent => 1, ForceArray  =>[]  );
+our $names_data = $xml->XMLin(   "../names.xml", ForceContent => 1, ForceArray  =>[]  );
 
+#########################################################################
+# First thing we need to do is establish a city skeleton of information,
+# then fill it in as needed by each subsection of the sample text.
+#########################################################################
+our $city=build_city($q->param('cityid'));
+
+
+
+if (defined $q->param('debug')) {
+print "seed: $city->{'seed'}\n";
+}
 our $height=500;
 our $width=500;
-our $xml_data = $xml->XMLin(   "../data.xml", ForceContent => 1, ForceArray  =>[]  );
 
 our $img = GD::Image->new($width,$height);
 our $palette = {
@@ -25,12 +55,10 @@ our $palette = {
     'wall'  => $img->colorAllocate(97,97,97),
 };
 
-
 &draw_ocean();
-&select_land_pattern();
-
+my $poly= &select_land_pattern();
 &city_size();
-
+&place_city();
 
 
 
@@ -40,7 +68,7 @@ our $palette = {
 if (defined $q->param('debug')) {
     exit;
 }
-$img->string(GD::gdLargeFont,2,10,"$seed",$palette->{'wall'});
+$img->string(GD::gdLargeFont,2,10,"$city->{'seed'}",$palette->{'wall'});
 &finish();
 exit;
 
@@ -51,12 +79,14 @@ exit;
 sub city_size {
 
 
+}
+sub place_city{
 
 
 }
 
-
 sub select_land_pattern{
+
     my $poly = new GD::Polygon;
     my @sides=('north', 'south', 'east', 'west','northeast' );
     my $side=$q->param('side') ;
@@ -65,39 +95,38 @@ sub select_land_pattern{
         $side=rand_from_array(\@sides);
     }
 
-    if ($q->param('loc') eq "on the coast"){
-        my $xbase=0;
-        my $ybase=0;
-        my $xcur=0;
-        my $ycur=0;
-        my $length=0;
+    #FIXME coast is currently the only one supported, hence the "or 1"
+    if ($city->{'location'}->{'name'} eq "on the coast"  or 1){
+        # Set base distances
+        my ($xbase, $ybase, $xcur, $ycur, $length)= (0,0,0,0,0);
         my $totaldistance=$width;
 
-    if ($side eq 'north' || $side eq 'south'){
-            $totaldistance=$width;
+        #Determine the total distance that needs to be traveled.
+        if ($side eq 'north' || $side eq 'south'){
+                $totaldistance=$width;
+        }elsif ($side eq 'east' || $side eq 'west'){
+                $totaldistance=$height;
+        }elsif($side eq 'northeast'){
+                $totaldistance=int (sqrt( $height**2 + $width**2) );
+        }
 
-    }elsif ($side eq 'east' || $side eq 'west'){
-            $totaldistance=$height;
-    }elsif($side eq 'northeast'){
-            $totaldistance=int (sqrt( $height**2 + $width**2) );
-    }
-
-        if ($side eq 'north'){
-            $poly->addPt(0,0);
-        } elsif ($side eq 'south'){
-            $ybase=$height;
+        # Determine the starting point for that side
+        if ($side eq 'south'){
             $poly->addPt(0,$height);
         } elsif ($side eq 'east'){
-            $xbase=$width;
             $poly->addPt($width,0);
-        } elsif ($side eq 'west'){
-            $poly->addPt(0,0);
-        } elsif ($side eq 'northeast'){
+        }elsif($side eq 'north' || $side eq 'west' || $side eq 'northeast'){
             $poly->addPt(0,0);
         }
 
+        # For "backward" sides, set the base to the opposite of 0
+        if ($side eq 'south'){
+            $ybase=$height;
+        }elsif ($side eq 'east'){
+            $xbase=$width;
+        }
+    
         while ($length < $totaldistance){
-
             my $lengthmod=  &d(20)-5;
             my $depthmod=   &d(24)-12;
 
@@ -111,10 +140,7 @@ sub select_land_pattern{
                 $ycur+=&d(20)-5;
                 $xcur+=&d(20)-5;
                 $length=max($xcur, $ycur);
-
             }
-
-
             if ($side eq 'north'){
                 $ycur=max( $ycur+$depthmod, $ybase-50);
             } elsif ($side eq 'south'){
@@ -126,41 +152,41 @@ sub select_land_pattern{
             } elsif ($side eq 'northeast'){
                 $ycur=max( $ycur+$depthmod, $ybase-50);
                 $xcur=max( $xcur+$depthmod, $xbase-50);
-    
+        
             }
             if (defined $q->param('debug')  ){
             }
             $poly->addPt($xbase+$xcur,$ybase+$ycur);
         }
-    }
-
-    if ($side eq 'north'){
-        $poly->addPt($width,$height);
-        $poly->addPt(0,$height);
-        $poly->offset(0,100);
-    } elsif ($side eq 'south'){
-        $poly->addPt($width,0);
-        $poly->addPt(0,0);
-        $poly->offset(0,-100);
-    } elsif ($side eq 'east'){
-        $poly->addPt(0,$height);
-        $poly->addPt(0,0);
-        $poly->offset(-100,0);
-    } elsif ($side eq 'west'){
-        $poly->addPt($width,$height);
-        $poly->addPt($width,0);
-        $poly->offset(100,0);
-    } elsif ($side eq 'northeast'){
-        $poly->addPt($width,$height+50);
-        $poly->addPt(-50,$height+50);
-        $poly->addPt(-50,-50);
-        $poly->offset(50,-50);
-    }
-
-    if (defined $q->param('debug')  ){
-        print Dumper $poly;
-    }
+        
+        if ($side eq 'north'){
+            $poly->addPt($width,$height);
+            $poly->addPt(0,$height);
+            $poly->offset(0,100);
+        } elsif ($side eq 'south'){
+            $poly->addPt($width,0);
+            $poly->addPt(0,0);
+            $poly->offset(0,-100);
+        } elsif ($side eq 'east'){
+            $poly->addPt(0,$height);
+            $poly->addPt(0,0);
+            $poly->offset(-100,0);
+        } elsif ($side eq 'west'){
+            $poly->addPt($width,$height);
+            $poly->addPt($width,0);
+            $poly->offset(100,0);
+        } elsif ($side eq 'northeast'){
+            $poly->addPt($width,$height+50);
+            $poly->addPt(-50,$height+50);
+            $poly->addPt(-50,-50);
+            $poly->offset(50,-50);
+        }
+        
     
+  }
+
+  print Dumper $poly if (defined $q->param('debug')  );
+
   $img->filledPolygon($poly,$palette->{'grass'});
  return $poly;
 }
@@ -178,149 +204,4 @@ sub finish {
     # Convert the image to PNG and print it on standard output
     print $img->png;
 }
-#######################################################################################################################
-################                                                                                       ################
-################                            These are more generic functions                           ################
-################                                                                                       ################
-#######################################################################################################################
-#######################################################################################################################
-#######################################################################################################################
-#######################################################################################################################
-
-
-###############################################################################
-#
-# set_seed - check the parameters for cityID and set the seed accordingly.
-# This is what allows us to return to previously generated hosts.
-#
-###############################################################################
-sub set_seed{
-    if (defined $q->param('cityid') and  $q->param('cityid')=~/(\d+)/){
-        return $1;
-    }else{
-        return int rand(1000000);
-    }
-}
-
-
-###############################################################################
-#
-# rand_from_array - select a random item from an array.
-#
-###############################################################################
-sub rand_from_array {
-    my ($array) = @_;
-    my $index = int( rand( scalar @{ $array} ) );
-    return $array->[$index];
-}
-
-###############################################################################
-#
-# roll_from_array - When passed a roll and a list of items, check the
-# min and max properties of each and select the one that $roll best fits
-# otherwise use the first item.
-# 
-###############################################################################
-sub roll_from_array {
-    my ( $roll, $items ) = @_;
-    my $selected_item = $items->[0];
-    for my $item (@$items) {
-        if (defined $item->{'min'} and defined $item->{'max'} ){
-            if ( $item->{'min'} <= $roll and $item->{'max'} >= $roll ) {
-                $selected_item = $item;
-                last;
-            }
-        }elsif ( defined $item->{'min'}   and ! defined $item->{'max'} ){
-            if ( $item->{'min'} <= $roll ) {
-                $selected_item = $item;
-                last;
-            }
-        }elsif ( ! defined $item->{'min'} and   defined $item->{'max'} ){
-            if ( $item->{'max'} >= $roll ) {
-                $selected_item = $item;
-                last;
-            }
-        }elsif ( ! defined $item->{'min'} and !  defined $item->{'max'} ){
-                $selected_item = $item;
-                last;
-        }
-    }
-    return $selected_item;
-}
-###############################################################################
-#
-# d - this serves the function of rolling a dice- a d6, d10, etc.
-#
-###############################################################################
-sub d {
-    my ($die) = @_;
-    # d as in 1d6
-    if ($die =~/^\d+$/){
-        return int( rand($die) );
-    }elsif ($die=~/^(\d+)d(\d+)$/){
-        my $dicecount=$1;
-        my $die=$2;
-        my $total=0;
-        while ($dicecount-- >0){
-            $total+=&d($die);
-        }
-        return $total;
-    }else{
-        return 1;
-    }
-}
-
-#####################################################
-#
-# Parse Object - a horribly named subroutine to parse
-# out and randomly select the parts.
-#
-#####################################################
-sub parse_object {
-    my ($object)=@_;
-    my $newobj= { 'content'=>'' };
-    # We currently only care about 4 parts
-    foreach my $part (qw/title pre root post trailer/){
-        # Make sure that the part exists for this object.
-        if(defined $object->{$part}){
-
-            my $newpart;
-            # If the object is an array, we're going to shuffle
-            # the array and select one of the elements.
-            if ( ref($object->{$part}) eq 'ARRAY'){
-                # Shuffle the array and pop one element off
-                my @parts=shuffle( @{$object->{$part}});
-                $newpart=pop(@parts);
-
-            # If the object is a Hash, we presume that there's only one choice
-            } elsif ( ref($object->{$part}) eq 'HASH'  and $object->{$part}->{'content'}){
-                # rename for easier handling
-                $newpart=$object->{$part};
-            }
-
-            # make sure the element has content;
-            # ignore it if it doesn't.
-            if (defined $newpart->{'content'}){
-                if (
-                        # If no chance is defined, add it to the list.
-                        (!defined $object->{$part.'_chance'}) or
-                        # If chance is defined, compare it to
-                        # the roll, and add it to the list.
-                        (defined $object->{$part.'_chance'} and &d(100) <= $object->{$part.'_chance'}) ) {
-
-                    $newobj->{$part}=$newpart->{'content'};
-                    if ($part eq 'title'){
-                        $newpart->{'content'}="$newpart->{'content'} " ;
-                    }elsif ($part eq 'trailer'){
-                        $newpart->{'content'}=" $newpart->{'content'}" ;
-                    }
-                    $newobj->{'content'}.= $newpart->{'content'};
-                }
-            }
-        }
-    }
-    # return the slimmed down version
-    return $newobj;
-}
-
 
