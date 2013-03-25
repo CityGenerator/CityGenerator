ad2e0c42bc327e63cbc411256f6a617226507d91
Jesse Morgan <morgajel@gmail.com>
2013-03-24 23:16:06 -0400

    reenabling tests

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -4,18 +4,18 @@ use strict;
 use lib "lib/";
 use lib "tests/";
 use Test::More;
-#use TestGenericGenerator;
-#use TestFlagGenerator;
-#use TestRegionGenerator;
-#use TestContinentGenerator;
+use TestGenericGenerator;
+use TestFlagGenerator;
+use TestRegionGenerator;
+use TestContinentGenerator;
 use TestNPCGenerator;
+use TestConditionGenerator;
+use TestMilitaryGenerator;
+use TestTavernGenerator;
+use TestEventGenerator;
+use TestDistrictGenerator;
+use TestMythGenerator;
 use TestCityGenerator;
-#use TestConditionGenerator;
-#use TestMilitaryGenerator;
-#use TestTavernGenerator;
-#use TestEventGenerator;
-#use TestDistrictGenerator;
-#use TestMythGenerator;
 
 
 done_testing();
