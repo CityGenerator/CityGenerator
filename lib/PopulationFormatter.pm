223e1cf81f22944bb29fc13f5d789b1612e984b5
Jesse Morgan <morgajel@gmail.com>
2013-07-30 15:29:22 -0400

    fix plural bug for races and removed spaces

--- lib/PopulationFormatter.pm
+++ lib/PopulationFormatter.pm
@@ -28,6 +28,8 @@ use CGI;
 use Data::Dumper;
 use Exporter;
 use List::Util 'shuffle', 'min', 'max';
+use Lingua::EN::Inflect qw(A PL_N);
+
 use POSIX;
 use version;
 
@@ -52,7 +54,7 @@ sub printRaces {
     $content .= "<ul> \n";
     foreach my $race ( reverse sort { $a->{'percent'} <=> $b->{'percent'} } @{ $city->{'races'} } ) {
         $content
-            .= "<li style='margin-left:200px;'>$race->{'population'} $race->{'plural'} ($race->{'percent'}%) </li> \n";
+            .= "<li style='margin-left:200px;'>$race->{'population'} ".PL_N($race->{'race'}, $race->{'population'})." ($race->{'percent'}%) </li> \n";
     }
     $content .= "</ul>";
 
