4315d4488c50c4f687e7e8f8fc36515d9287bf02
braddoro <braddoro@gmail.com>
2013-08-10 12:46:37 -0400

    Establishment generator narrrative formatting.

--- lib/LocaleFormatter.pm
+++ lib/LocaleFormatter.pm
@@ -28,7 +28,7 @@ use CGI;
 use Data::Dumper;
 use Exporter;
 use List::Util 'shuffle', 'min', 'max';
-use Lingua::EN::Inflect qw( A );
+use Lingua::EN::Inflect qw( A PL_N );
 use POSIX;
 use version;
 
@@ -98,6 +98,79 @@ sub describe_tavern {
     $content
         .= "The law $tavern->{'law'} the tavern and its patrons, however most violence is handled by $tavern->{'violence'}. \n";
     $content .= "</li>";
+    return $content;
+}
+
+###############################################################################
+
+=head2 printEstablishments()
+
+printTaverns strips out Establishment information and formats it.
+
+=cut
+
+###############################################################################
+sub printEstablishments {
+    my ($city) = @_;
+    my $content = "";
+    $content .= "foo";
+    if ( scalar( @{ $city->{'establishments'} } ) > 0 ) {
+        $content
+            .= "<p>These establishments worthy of mention in $city->{'name'}:</p>\n";
+        $content .= "<ul class='demolistfinal'>";
+        foreach my $establishment ( @{ $city->{'establishments'} } ) {
+            $content .= describe_establishment($establishment);
+        }
+
+        $content .= "</ul>";
+    } else {
+        $content .= "<p>There are no establishments in this town.</p>\n";
+    }
+
+
+    return $content;
+}
+
+sub describe_establishment {
+    # The Fat Ostrich is a small, clean, rundown bakery made of stone and wood and is in a rundown area of 
+    # the red light district.  It appears to be busy, has five patrons inside, and has a good reputation 
+    # with the locals for cheap prices and quality work.  It is run by Jesse Husband-Father who is a large 
+    # Orc with an even temperament but prone to occasional bursts of rage.  The bakery is on good terms 
+    # with the law but regularly pays bribes to organized crime lord Brad Crime-Boss.    
+    
+    my ($establishment) = @_;
+    my $content = "<li>";
+    $content .= "<b>The $establishment->{'name'}</b> A( $establishment->{'type'} ) that $establishment->{'popularity_description'} ";
+    $content .= "$establishment->{'size_description'}  $establishment->{'reputation_description'} \n";
+    $content .= " and is run by a $establishment->{'manager'}->{'sex'} A( $establishment->{'manager'}->{'race'} ) ";
+    $content .= "named $establishment->{'manager'}->{'name'} ";
+
+#use Lingua::EN::Inflect qw( A PL_N );
+#
+#$content .= "Laws are enforced by " . A( $city->{'laws'}->{'enforcer'} ) . ", $city->{'laws'}->{'enforcement'}. \n";
+#
+#$content .= "The imprisonment rate is $city->{'imprisonment_rate'}->{'percent'}% of the population ($city->{'imprisonment_rate'}->{'population'} "
+#. PL_N( "adult", $city->{'imprisonment_rate'}->{'population'} ) . "). \n";
+    
+#            
+#    $establishment->{'manager'}->{'behavior'}
+    
+# is run by $establishment->{'manager'}        
+#        . A( $establishment->{'size_description'} )
+#        . " establishment that $establishment->{'popularity_description'}. \n";
+#    $content .= "It has a reputation for $establishment->{'reputation_description'}.\n";
+#
+#    if ( defined $establishment->{'bartender'}->{'name'} ) {
+#        $content .= "The $establishment->{'name'} is owned by a $establishment->{'bartender'}->{'behavior'} $establishment->{'bartender'}->{'race'}" 
+#        . "named $establishment->{'bartender'}->{'name'} whose prices are $establishment->{'cost_description'} and the patrons appear $establishment->{'atmosphere'}. \n";
+#    } else {
+#        $content .= "The $establishment->{'name'} is run by a $establishment->{'bartender'}->{'behavior'} $establishment->{'bartender'}->{'race'}"
+#        . "whose prices are $establishment->{'cost_description'} and the patrons seem $establishment->{'atmosphere'}. \n";
+#    }
+#    $content
+#        .= "The law $establishment->{'law'} the establishment and its patrons, however most violence is handled by $establishment->{'violence'}. \n";
+
+        $content .= "</li>";
     return $content;
 }
 
