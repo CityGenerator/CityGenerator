3173d1d7f42e621896b8d9da598a8a1e02363134
braddoro <braddoro@gmail.com>
2013-08-25 09:39:59 -0400

    Removed dead code and formatted fro use with city generator.

--- lib/ResourceGenerator.pm
+++ lib/ResourceGenerator.pm
@@ -1,23 +1,29 @@
 #!/usr/bin/perl -wT
+package ResourceGenerator;
+
 use strict;
+use warnings;
+use vars qw(@ISA @EXPORT_OK $VERSION $XS_VERSION $TESTING_PERL_ONLY);
+use base qw(Exporter);
+@EXPORT_OK = qw(create_resource);
+
+
+use Carp;
+use CGI;
 use Data::Dumper;
+use Exporter;
+use GenericGenerator qw(set_seed rand_from_array roll_from_array d parse_object seed);
+use NPCGenerator;
+use List::Util 'shuffle', 'min', 'max';
+use Lingua::EN::Titlecase;
+use POSIX;
+use version;
 use XML::Simple;
 use lib "lib/";
-use List::Util 'shuffle', 'min', 'max' ;
-use POSIX;
-use GenericGenerator;
-use GenericGenerator qw( rand_from_array roll_from_array d parse_object seed);
-use NPCGenerator;
-local $ENV{XML_SIMPLE_PREFERRED_PARSER} = 'XML::Parser';
 
+local $ENV{XML_SIMPLE_PREFERRED_PARSER} = 'XML::Parser';
 my $xml = XML::Simple->new();
 my $xml_data = $xml->XMLin( "xml/resources.xml", ForceContent => 1, ForceArray => ['option'] );
-
-#print Dumper $xml_data;
-#(11:59:46 AM) Jesse Morgan: $city->{'resources'}=[]
-#(12:00:07 PM) Jesse Morgan: push @{$city->{'resources'} } , ResourceGenerator::create_resource();
-# my $resources->{'resources'}=[]
-# $resources->{'resources'}=[]
 
 ###############################################################################
 
@@ -37,39 +43,29 @@ This method is used to create a resource
 
 ###############################################################################
 sub create_resource {
-my $loop = 0;
-my $max = d(3);
-my $output = '';
-my $roll;
-    while ($loop < $max) {
-       
-        $roll = d(3);
-        
-        if($roll == 1){ # Wildlife
-            $output = create_wildlife();   
-        }
-    
-        if($roll == 2){ # Natural
-            $output = create_natural();       
-        }
-    
-        if($roll == 3){ # Structure
-            $output = create_structure();   
-        }
-        
-        if(d(1) == 1){ 
-            $output .= add_visitors();
-        }else{
-            $output .= '. ';
-        }
-        
-        if(d(6) < 5){ 
-            $output .= add_precious();    
-        }
-        
-        #print Dumper $output;
-        
-        $loop++;
+	my $output;
+	my $roll = d(3);
+
+    if($roll == 1){ # Wildlife resource
+        $output = create_wildlife();
+    }
+
+    if($roll == 2){ # Natural resource
+        $output = create_natural();
+    }
+
+    if($roll == 3){ # Structure resource
+        $output = create_structure();
+    }
+
+    if(d(1) == 1){
+        $output .= add_visitors();
+    }else{
+        $output .= '. ';
+    }
+
+    if(d(6) < 5){
+        $output .= add_precious();
     }
 }
 
@@ -86,12 +82,12 @@ add precious stuff to the narrative
 ###############################################################################
 sub create_structure {
     my ($content) = @_;
-    
+
     $content = ucfirst(rand_from_array( $xml_data->{'structures'}->{'age'}->{'option'} )->{'content'}) .' ';
     $content .= rand_from_array( $xml_data->{'structures'}->{'option'} )->{'content'} .' ';
     $content .= 'inhabited by ';
     $content .= rand_from_array( $xml_data->{'wildlife'}->{'type'} )->{'content'} .' '. rand_from_array(    $xml_data->{'wildlife'}->{'animal'} )->{'content'} .'s';
-    
+
     return $content;
 }
 
@@ -107,7 +103,7 @@ add precious stuff to the narrative
 sub create_natural {
     my ($content) = @_;
     my $natural;
-    
+
     $natural = rand_from_array( $xml_data->{'natural'}->{'type'} );
     $content = '';
     if(d(2) == 1){
@@ -118,7 +114,7 @@ sub create_natural {
     }
     $content .= $natural->{'content'} . ' ';
     $content .= rand_from_array( $natural->{'suffix'}->{'option'} )->{'content'};
-    
+
     return ucfirst($content);
 }
 
@@ -134,7 +130,7 @@ add precious stuff to the narrative
 sub create_wildlife {
     my ($content) = @_;
     my $wildlife;
-    
+
     $content = '';
     if(d(2) == 1){
         $content .= roll_from_array( d(100), $xml_data->{'scarcity'}->{'option'} )->{'content'} . ' ';
@@ -145,7 +141,7 @@ sub create_wildlife {
     $wildlife = rand_from_array( $xml_data->{'wildlife'}->{'animal'} );
     $content .= $wildlife->{'content'} . ' ' ;
     $content .= $wildlife->{'group'};
-    
+
     return ucfirst($content);
 }
 
@@ -163,23 +159,13 @@ sub add_precious {
     my $who = '';
 
     if(d(2) == 1){
-        $who = NPCGenerator::create_npc()->{'name'} . ' said ';    
+        $who = NPCGenerator::create_npc()->{'name'} . ' said ';
     }
     $precious = ' ' . ucfirst($who . rand_from_array( $xml_data->{'precious'}->{'find'} )->{'content'}) . ' ' .
     rand_from_array( $xml_data->{'precious'}->{'type'} )->{'content'} . ' ' .
-    rand_from_array( $xml_data->{'precious'}->{'option'} )->{'content'} . ' ' . 
+    rand_from_array( $xml_data->{'precious'}->{'option'} )->{'content'} . ' ' .
     rand_from_array( $xml_data->{'precious'}->{'post'} )->{'content'} . '.';
 
-#    if (defined $rumor->{'heardit'}){
-#       $rumor->{'template'}=$rumor->{'heardit'}." ".$rumor->{'template'};
-#    }
-#
-#    $rumor->{'template'}=ucfirst $rumor->{'template'};
-#    
-#    if (defined $rumor->{'belief'}){
-#       $rumor->{'template'}=$rumor->{'template'}." ".$rumor->{'belief'};
-#    }
-    
     return $precious;
 }
 
@@ -199,10 +185,36 @@ sub add_visitors {
     $visitors .= rand_from_array( $xml_data->{'feelings'}->{'option'} )->{'content'};
     $visitors .= ' by ';
     $visitors .= rand_from_array( $xml_data->{'groups'}->{'option'} )->{'content'} . '.';
-   
+
     return $visitors;
 }
 
 
 1;
 __END__
+
+=head1 AUTHOR
+
+Jesse Morgan (morgajel)  C<< <morgajel@gmail.com> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2013, Jesse Morgan (morgajel) C<< <morgajel@gmail.com> >>. All rights reserved.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation version 2
+of the License.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+=head1 DISCLAIMER OF WARRANTY
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+=cut
