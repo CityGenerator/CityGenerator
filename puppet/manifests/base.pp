61a2855f5cabc48001738e5baaceafc58f32c3b9
morgajel <morgajel@gmail.com>
2013-11-10 02:00:38 -0500

    Getting closer; httpd works, but apps do not.

--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -1,6 +1,7 @@
 node default {
 
     #include nginx
+    include iptables
     include users
     include httpd
     include repositories
--- puppet/modules/httpd/manifests/install.pp
+++ puppet/modules/httpd/manifests/install.pp
@@ -1,6 +1,6 @@
 class httpd::install {
 
-    package{ ["httpd"]:
+    package{ ["httpd",'perl-CGI','perl-XML-Simple', 'perl-Template-Toolkit','perl-JSON','perl-Lingua-EN-Inflect', 'perl-Lingua-EN-Numericalize', 'perl-Lingua-EN-Inflect-Number', 'perl-Test-Harness']:
         ensure =>present,
     }
 }
